965420a0a3def69c30128dfa035e118112cb287d
chestnut1693 <chestnut1693@gmail.com>
2020-01-10 16:28:08 +0100

    change default example to batslocator

--- src/test/java/batslocator/BatsLocatorPluginTest.java
+++ src/test/java/batslocator/BatsLocatorPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package batslocator;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class BatsLocatorPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(BatsLocatorPlugin.class);
 		RuneLite.main(args);
 	}
 }
