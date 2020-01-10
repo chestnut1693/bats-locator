34eb205d7867b6627225f3ef8833a605080c416d
chestnut1693 <chestnut1693@gmail.com>
2020-01-10 16:40:48 +0100

    change example skeleton to batslocator skeleton

--- src/main/java/batslocator/BatsLocatorConfig.java
+++ src/main/java/batslocator/BatsLocatorConfig.java
@@ -1,19 +1,47 @@
 package batslocator;
 
+import java.awt.Color;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.Range;
 
-@ConfigGroup("example")
+@ConfigGroup("batslocator")
 public interface BatsLocatorConfig extends Config
 {
 	@ConfigItem(
-		keyName = "greeting",
-		name = "Welcome Greeting",
-		description = "The message to show to the user when they login"
+		keyName = "batsColor",
+		name = "Bats chest color",
+		description = "Configures the color of the bats chest dot and number.",
+		position = 1
 	)
-	default String greeting()
+	default Color batsColor()
 	{
-		return "Hello";
+		return Color.white;
+	}
+
+	@ConfigItem(
+		keyName = "poisonColor",
+		name = "Poison chest color",
+		description = "Configures the color of the poison chest dot and number",
+		position = 1
+	)
+	default Color poisonColor()
+	{
+		return Color.green;
+	}
+
+	@Range(
+		max = 255
+	)
+	@ConfigItem(
+		keyName = "dotTransparency",
+		name = "Dot transparency",
+		description = "Configures the transparency of the chest dots that are not likely to contain poison or bats",
+		position = 2
+	)
+	default int dotTransparency()
+	{
+		return 75;
 	}
 }
--- src/main/java/batslocator/BatsLocatorPlugin.java
+++ src/main/java/batslocator/BatsLocatorPlugin.java
@@ -3,18 +3,17 @@ package batslocator;
 import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
-import net.runelite.api.GameState;
-import net.runelite.api.events.GameStateChanged;
 import net.runelite.client.config.ConfigManager;
-import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Bats Locator",
+	description = "Helps locating bats in the thieving room of Chambers of Xeric",
+	tags = {"finder", "thieving"},
+	enabledByDefault = false
 )
 public class BatsLocatorPlugin extends Plugin
 {
@@ -24,30 +23,19 @@ public class BatsLocatorPlugin extends Plugin
 	@Inject
 	private BatsLocatorConfig config;
 
+	@Provides
+	BatsLocatorConfig provideConfig(ConfigManager configManager)
+	{
+		return configManager.getConfig(BatsLocatorConfig.class);
+	}
+
 	@Override
 	protected void startUp() throws Exception
 	{
-		log.info("Example started!");
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
-		log.info("Example stopped!");
-	}
-
-	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
-	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
-		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
-		}
-	}
-
-	@Provides
-	BatsLocatorConfig provideConfig(ConfigManager configManager)
-	{
-		return configManager.getConfig(BatsLocatorConfig.class);
 	}
 }
